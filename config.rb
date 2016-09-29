98558d1e68a098143ffdffbc1e26a99e7adfade6
Matt Cloyd <cloydster@gmail.com>
2016-09-29 17:23:02 -0400

    Fix deploy method_missing error
    Why:
    
    We were getting an error that 'Setting method doesn't exist', but it
    wasn't telling us which method it was.
    
    Turns out, the simple `deploy.method` has been changed to
    `deploy.deploy_method`.
    
    Thanks to:
    
    https://github.com/middleman-contrib/middleman-deploy/issues/100

--- config.rb
+++ config.rb
@@ -102,13 +102,13 @@ end
 
 
 activate :deploy do |deploy|
-  deploy.method = :git
+  deploy.deploy_method = :git
   deploy.branch = 'gh-pages' # or master
 
   committer_app = "#{Middleman::Deploy::PACKAGE} v#{Middleman::Deploy::VERSION}"
   commit_message = "Deployed using #{committer_app}"
 
-  if ENV["TRAVIS_BUILD_NUMBER"] then
+  if ENV["TRAVIS_BUILD_NUMBER"]
     commit_message += " (Travis Build \##{ENV["TRAVIS_BUILD_NUMBER"]})"
   end
 
